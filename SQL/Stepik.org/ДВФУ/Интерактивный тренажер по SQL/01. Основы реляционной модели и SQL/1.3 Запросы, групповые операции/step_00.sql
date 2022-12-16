87d5c88ac458aa8001a54fc4557fefff4e1a6428
tskdvraz0r <ts.kdv.raz0r@ya.ru>
2022-12-16 14:22:52 +0300

    added new MySQL query

---
+++ SQL/Stepik.org/ДВФУ/Интерактивный тренажер по SQL/01. Основы реляционной модели и SQL/1.3 Запросы, групповые операции/step_00.sql
@@ -0,0 +1,5 @@
+-- Active: 1671137145177@@127.0.0.1@3306@interactive_sql
+
+--16.12.2022
+INSERT INTO book(title, author, price, amount)
+    VALUES('Игрок',	'Достоевский Ф.М.',	480.50,	10)
---
+++ SQL/Stepik.org/ДВФУ/Интерактивный тренажер по SQL/01. Основы реляционной модели и SQL/1.3 Запросы, групповые операции/step_02 - Выбор уникальных элементов столбца.sql
@@ -0,0 +1,8 @@
+-- Active: 1671137145177@@127.0.0.1@3306@interactive_sql
+/* Отобрать различные (уникальные) элементы столбца amount таблицы book. */
+
+--16.12.2022
+SELECT
+    DISTINCT amount
+
+FROM book
