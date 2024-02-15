b7ff233f3bd3b175a15647b89245f199ef859030
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-02-15 22:53:10 +0300

    [c/cpp] feat: Решена задача 1.4.9

---
+++ stepik.org/sergey_balakirev/good_good_c_cpp/modules/module_01/submodule_1_4/step_09.c
@@ -0,0 +1,11 @@
+// Date: 15.02.2024
+// Step: 1.4.9
+// Url: https://stepik.org/lesson/1191239/step/9?unit=1204210
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("I love C\n");
+    return 0;
+}
