f481ed1f4e1b2f9362139fb4b84c23841f44345f
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-02-15 22:53:36 +0300

    [c/cpp] feat: Решена задача 2.2.6

---
+++ stepik.org/sergey_balakirev/good_good_c_cpp/modules/module_02/submodule_2_2/step_06.c
@@ -0,0 +1,13 @@
+// Date: 15.02.2024
+// Step: 2.2.6
+// Url: https://stepik.org/lesson/1191244/step/6?unit=1204215
+
+int main(void)
+{
+    double var_d1;
+    double var_d2;
+
+    var_d1 = var_d2 = 0.5;
+
+    return 0;
+}
