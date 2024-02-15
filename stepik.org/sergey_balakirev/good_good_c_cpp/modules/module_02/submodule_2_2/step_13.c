549f11cd8512a66e038dc5d03a9b694ce98500b2
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-02-15 22:53:42 +0300

    [c/cpp] feat: Решена задача 2.2.13

---
+++ stepik.org/sergey_balakirev/good_good_c_cpp/modules/module_02/submodule_2_2/step_13.c
@@ -0,0 +1,12 @@
+// Date: 15.02.2024
+// Step: 2.2.13
+// Url: https://stepik.org/lesson/1191244/step/13?unit=1204215
+
+int main(void)
+{
+    double var;
+    double res;
+
+    res = sizeof(var);
+    return 0;
+}
