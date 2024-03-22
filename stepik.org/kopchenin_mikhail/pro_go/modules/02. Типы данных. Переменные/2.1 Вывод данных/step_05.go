1e7618a0e6b1f04352bdee20ed76353466f3b9f3
ts.kdv.raz0r <ts.kdv.raz0r@ya.ru>
2024-03-23 01:27:59 +0300

    [GO] feat: Задача решена.

---
+++ stepik.org/kopchenin_mikhail/pro_go/modules/02. Типы данных. Переменные/2.1 Вывод данных/step_05.go
@@ -0,0 +1,10 @@
+// Date: 23.03.2024
+// Url: https://stepik.org/lesson/917011/step/5?unit=922790
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Здравствуй,")
+	fmt.Print("Иосиф!")
+}
---
+++ stepik.org/kopchenin_mikhail/pro_go/modules/02. Типы данных. Переменные/2.1 Вывод данных/step_09.go
@@ -0,0 +1,12 @@
+// Date: 23.03.2024
+// Url: https://stepik.org/lesson/917011/step/9?unit=922790
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("row1")
+	fmt.Println("row2")
+	fmt.Println("row3")
+	fmt.Print("row4")
+}
