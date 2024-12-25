e45194b0a45dd3ca968145021990bb3972a11d85
SotransAnalyticsDep <analytics_dep@sotrans.ru>
2024-12-25 16:12:08 +0300

    Задача решена;

---
+++ stepik/pavel_khoshev/01_course_multithreading_python/module/module_01/lesson_11/step_14.py
@@ -0,0 +1,37 @@
+import threading as th
+
+
+initial_bid: int = 0
+bid_increment: int = 1
+max_bid: int = 10
+interval: int = 1
+current_bid: int = initial_bid
+
+
+def increase_bid(bid_increment: int, max_bid: int, interval: int) -> None:
+    global current_bid
+
+    if current_bid < max_bid:
+        current_bid += bid_increment
+        print(f"Текущая ставка: {current_bid} у.е.")
+
+        th.Timer(
+            interval=interval,
+            function=increase_bid,
+            kwargs={
+                "bid_increment": bid_increment,
+                "max_bid": max_bid,
+                "interval": interval,
+            },
+        ).start()
+
+    else:
+        print("Ставок нет, аукцион завершен!")
+
+
+thread: th.Thread = th.Thread(
+    target=increase_bid,
+    kwargs={"bid_increment": bid_increment, "max_bid": max_bid, "interval": interval},
+)
+thread.start()
+thread.join()
---
+++ stepik/pavel_khoshev/01_course_multithreading_python/module/module_01/lesson_11/step_15.py
@@ -0,0 +1,16 @@
+import time
+import threading as th
+
+
+astronauts: list[str] = ["Алексей Леонов", "Юрий Гагарин", "Джон Гленн"]
+tasks: list[str] = ["Ремонт оборудования", "Проведение экспериментов", "Мониторинг систем"]
+intervals: list[float] = [0.7, 1.3, 1.8]
+
+
+def worker(interval: float, astronaut: str, task: str) -> None:
+    time.sleep(interval)
+    print(f"{astronaut} выполняет задачу: {task}")
+
+
+for interval, astronaut, task in zip(intervals, astronauts, tasks):
+    th.Thread(target=worker, args=(interval, astronaut, task)).start()
