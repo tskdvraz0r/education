5ddc2f41fbb9b736114ed34c5f48f31821c3cf6a
Dmitry ts.kdv.raz0r Kolganov <ts.kdv.raz0r@ya.ru>
2025-01-09 09:09:05 +0300

    Рефакторинг;

--- stepik/pavel_khoshev/01_course_multithreading_python/module/module_05/lesson_01/step_13.py
+++ stepik/pavel_khoshev/01_course_multithreading_python/module/module_05/lesson_01/step_13.py
@@ -1,35 +1,76 @@
-import time
-import threading as t
+import sys
+from collections.abc import Generator
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
+from threading import Lock, Thread
+from time import sleep
 
-oven_lock = t.Lock()
 
-cooks_names = ["Алексей", "Марина", "Сергей", "Ирина", "Николай"]
-pizzas = {
+sys.path.append("/Users/ts.kdv.raz0r/Yandex.Disk.localized/GitHub/education")
+from packages.utils.decorators.time_spend import time_spend  # type: ignore
+
+# Блокиратор "духовки";
+oven_lock: Lock = Lock()
+
+# Список имён поваров;
+cooks_names: list[str] = ["Алексей", "Марина", "Сергей", "Ирина", "Николай"]
+
+# Список видов пицц и время их приготовления;
+pizzas: dict[str, int] = {
     "Маргарита": 3,
     "Пепперони": 2,
     "Вегетарианская": 4,
     "Четыре сыра": 5,
-    "Гавайская": 3
+    "Гавайская": 3,
 }
 
-def create_pizze(cook: str, pizza_delay: tuple) -> None:
-    pizza, delay = pizza_delay
-    with oven_lock:
-        print(f'{cook} начал(а) готовить пиццу "{pizza}".')
-        time.sleep(delay)
-        print(f'{cook} закончил(а) готовить пиццу "{pizza}".')
 
+class Producer:
+    def __init__(self, queue: Queue[tuple[str, str, int]]) -> None:
+        self.queue: Queue[tuple[str, str, int]] = queue
+
+    def get_data(self) -> Generator[tuple[str, str, int]]:
+        for cook_name, pizza_name, delay in zip(
+            cooks_names, pizzas.keys(), pizzas.values()
+        ):
+            yield (cook_name, pizza_name, delay)
+
+    def produce(self) -> None:
+        for cook_name, pizza_name, delay in self.get_data():
+            self.queue.put(item=(cook_name, pizza_name, delay))
+
+
+class Consumer:
+    def __init__(self, queue: Queue[tuple[str, str, int]]) -> None:
+        self.queue: Queue[tuple[str, str, int]] = queue
+
+    def consume(self) -> None:
+        while not self.queue.empty():
+            cook_name, pizza_name, delay = self.queue.get()
+            with oven_lock:
+                print(f'{cook_name} начал(а) готовить пиццу "{pizza_name}".')
+                sleep(delay)
+                print(f'{cook_name} закончил(а) готовить пиццу "{pizza_name}".')
+            self.queue.task_done()
+
+
+@time_spend
 def main() -> None:
-    threads = [
-        t.Thread(target=create_pizze, args=(cook, pizza_delay))
-        for cook, pizza_delay in zip(cooks_names, pizzas.items())
-    ]
+    queue: Queue[tuple[str, str, int]] = Queue()
 
-    for thread in threads:
-        thread.start()
-        thread.join()
+    producer: Producer = Producer(queue=queue)
+    thread_producer = Thread(target=producer.produce)
+    thread_producer.start()
+    thread_producer.join()
+
+    consumer: Consumer = Consumer(queue=queue)
+
+    with ThreadPoolExecutor() as executor:
+        executor.submit(consumer.consume)
+    queue.join()
 
     print("Все пиццы приготовлены!")
+
 
 if __name__ == "__main__":
     main()
